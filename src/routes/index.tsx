93d3a0596f8eddb9622c4b12ab1239f8fd7fb7ff
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-04 22:31:06 +0900

    feat: add spinner

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,19 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import React from "react";
+import { Toaster } from "react-hot-toast";
 import { rpcClient } from "../rpc/client";
 import serverOnly from "../server-only";
 
 export function Page() {
+  return (
+    <React.Suspense>
+      <Toaster />
+      <PageInner />
+    </React.Suspense>
+  );
+}
+
+function PageInner() {
   const queryKey = ["getCounter"];
   const getCounterQuery = useQuery({
     queryKey,
@@ -20,19 +31,26 @@ export function Page() {
     },
   });
 
+  const loading = getCounterQuery.isLoading || updateCounterMutation.isLoading;
+
   return (
     <div className="flex flex-col gap-2 py-8 mx-auto w-full max-w-2xl">
       <div className="flex flex-col gap-2">
-        Counter = {getCounterQuery.data ?? "..."}
+        <div className="flex items-center gap-3">
+          <span>Counter = {getCounterQuery.data ?? "..."}</span>
+          {loading && <div className="antd-spin w-4 h-4"></div>}
+        </div>
         <div className="flex gap-2">
           <button
             className="antd-btn antd-btn-default px-2"
+            disabled={loading}
             onClick={() => updateCounterMutation.mutate(-1)}
           >
             -1
           </button>
           <button
             className="antd-btn antd-btn-default px-2"
+            disabled={loading}
             onClick={() => updateCounterMutation.mutate(+1)}
           >
             +1
