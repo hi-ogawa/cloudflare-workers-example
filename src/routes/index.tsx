e89a51a044b266d91cb97316af010d772690c77f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-04 17:57:45 +0900

    feat: add serverOnlyModulesPlugin (wip)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,16 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { rpcClient } from "../rpc/client";
+import { rpcRoutes } from "../rpc/server";
 
 export function Page() {
   const queryKey = ["getCounter"];
   const getCounterQuery = useQuery({
     queryKey,
     // TODO: absurd workaround to eliminate server code on client build
-    queryFn: import.meta.env.SSR
-      ? () => import("../rpc/server").then((lib) => lib.rpcRoutes.getCounter())
-      : rpcClient.getCounter,
+    // queryFn: import.meta.env.SSR
+    //   ? () => import("../rpc/server").then((lib) => lib.rpcRoutes.getCounter())
+    //   : rpcClient.getCounter,
+    queryFn: import.meta.env.SSR ? rpcRoutes.getCounter : rpcClient.getCounter,
     suspense: import.meta.env.SSR,
   });
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import importIndexHtmlPlugin from "@hiogawa/vite-import-index-html";
 import vaviteConnect from "@vavite/connect";
 import react from "@vitejs/plugin-react";
 import unocss from "unocss/vite";
-import { defineConfig } from "vite";
+import { Plugin, defineConfig } from "vite";
 
 export default defineConfig((ctx) => ({
   plugins: [
+    serverOnlyModulesPlugin(),
     react(),
     unocss(),
     importIndexHtmlPlugin(),
@@ -31,3 +32,27 @@ export default defineConfig((ctx) => ({
   },
   clearScreen: false,
 }));
+
+function serverOnlyModulesPlugin(): Plugin {
+  return {
+    name: serverOnlyModulesPlugin.name,
+    enforce: "pre",
+    async resolveId(source, importer, options) {
+      console.log({ source, importer, options });
+      // const resolved = await this.resolve(source, importer, options);
+      // if (resolved) {
+      //   if (resolved.id === "/src/rpc/server.ts") {
+      //     // return { }
+      //   }
+      // }
+    },
+    transform(_code, id, options) {
+      if (!options?.ssr) {
+        if (id.endsWith("/src/rpc/server.ts")) {
+          return `module.exports = {}`;
+        }
+      }
+      return undefined;
+    },
+  };
+}
