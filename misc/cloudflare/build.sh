4c7f203cdbf4ce96ecd0b12eb41b124c8c740e21
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-05 15:08:55 +0900

    chore: add preview kv

--- misc/cloudflare/build.sh
+++ misc/cloudflare/build.sh
@@ -15,9 +15,9 @@ set -eu -o pipefail
 rm -rf dist/cloudflare
 mkdir -p dist/cloudflare/bucket
 
-# dist/cf/index.js
+# dist/cloudflare/index.js
 esbuild dist/server/index.js --outfile=dist/cloudflare/index.js --metafile=dist/esbuild-metafile.json \
   --bundle --minify --format=esm --platform=browser --external:__STATIC_CONTENT_MANIFEST
 
-# dist/cf/bucket
+# dist/cloudflare/bucket
 cp -r dist/client/assets dist/cloudflare/bucket/assets
