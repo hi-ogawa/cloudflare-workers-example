126f7bb4d71f941ecfb6f223b4638bfb6d054898
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-07 12:57:01 +0900

    refactor: minor

--- src/utils/worker-env.ts
+++ src/utils/worker-env.ts
@@ -2,19 +2,21 @@ import type { KVNamespace } from "@miniflare/kv";
 
 export let env: {
   kv: KVNamespace;
-};
+} = {} as any;
 
 export function setEnv(v: any) {
   env = v;
 }
 
-// setup kv for dev
-export async function initializeEnv() {
-  if (import.meta.env.PROD) {
-    return;
-  }
+async function setEnvDev() {
   const { KVNamespace } = await import("@miniflare/kv");
   const { FileStorage } = await import("@miniflare/storage-file");
-  const kv = new KVNamespace(new FileStorage(".wrangler/.vite-dev/kv"));
-  env = { kv };
+  env.kv = new KVNamespace(new FileStorage(".wrangler/.vite-dev/kv"));
+}
+
+// setup kv for dev
+export async function initializeEnv() {
+  if (import.meta.env.DEV) {
+    await setEnvDev();
+  }
 }
