ef19576f96119a0bf1614b40b53ec08d41754dcf
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-11 14:34:39 +0900

    refactor: simplify repl script

--- src/db/repl.ts
+++ src/db/repl.ts
@@ -1,27 +1,18 @@
-import repl from "node:repl";
 import { sql } from "kysely";
 import { db, initializeDb } from ".";
 import { setWorkerEnvDev } from "../utils/worker-env-dev";
 
-// usage
+// usage:
 // pnpm repl
 // > await sql`SELECT 1 + 1`.execute(db)
 // > await sql`PRAGMA table_list`.execute(db)
 // > await db.selectFrom("counter").selectAll().execute()
 
 async function main() {
-  console.log("* initializing environemtnt...");
+  console.log("* setting up globals...");
   await setWorkerEnvDev();
   await initializeDb();
-
-  console.log("* repl is ready");
-  const replServer = repl.start();
-  replServer.setupHistory("./node_modules/.cache/__repl-history.txt", (e) => {
-    if (e) {
-      console.error("* repl histroy error", e);
-    }
-  });
-  Object.assign(replServer.context, {
+  Object.assign(globalThis, {
     sql,
     db,
   });
