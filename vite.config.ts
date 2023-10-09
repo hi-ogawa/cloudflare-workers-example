d46858b79ce0d79f05c04fc10159f39e99d38de2
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-10-09 09:48:02 +0900

    chore(deps): use `vite-null-export` (#18)

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-import path from "node:path";
-import process from "node:process";
 import importIndexHtmlPlugin from "@hiogawa/vite-import-index-html";
+import { viteNullExportPlugin } from "@hiogawa/vite-null-export";
 import vaviteConnect from "@vavite/connect";
 import react from "@vitejs/plugin-react";
-import * as esModuleLexer from "es-module-lexer";
 import unocss from "unocss/vite";
-import { type Plugin, defineConfig } from "vite";
+import { defineConfig } from "vite";
 
 export default defineConfig((ctx) => ({
   plugins: [
-    emptyModulesPlugin({
-      serverOnly: [path.join(process.cwd(), "src/rpc/server.ts")],
+    viteNullExportPlugin({
+      serverOnly: ["**/rpc/server.ts"],
     }),
     react(),
     unocss(),
@@ -33,29 +31,3 @@ export default defineConfig((ctx) => ({
   },
   clearScreen: false,
 }));
-
-// similar to https://github.com/remix-run/remix/blob/80c6842f547b7e83b58f1963894b07ad18c2dfe2/packages/remix-dev/compiler/plugins/emptyModules.ts#L10
-// but vite needs to fake esm exports
-function emptyModulesPlugin(opts: { serverOnly: string[] }): Plugin {
-  return {
-    name: emptyModulesPlugin.name,
-    enforce: "pre",
-
-    // (ab)use this hook to do async init
-    async configResolved(_config) {
-      await esModuleLexer.init;
-    },
-
-    transform(code, id, transformOpts) {
-      if (!transformOpts?.ssr && opts.serverOnly.includes(id)) {
-        const [_import, exports] = esModuleLexer.parse(code);
-        return exports
-          .map((e) =>
-            e.n === "default" ? `export default {}` : `export var ${e.n} = {}`,
-          )
-          .join("\n");
-      }
-      return undefined;
-    },
-  };
-}
